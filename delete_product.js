dd6a8f15633c14453f31bdbd1568670c4d087af9
truptig-2512 <truptigorivale00@gmail.com>
2024-12-27 09:32:06 +0530

    10 commit

--- delete_product.js
+++ delete_product.js
@@ -2,8 +2,14 @@ fetch("http://localhost:8080/products")
   .then((data) => data.json())
   .then((data) => {
     console.log(data);
+    
+    document.write(`<p>totalElements:${data["page"]["totalElements"]}</p>`)
+    document.write(`<p>totalPages:${data["page"]["totalPages"]}</p>`)
+    document.write(`<p>number:${data["page"]["number"]}</p>`)
+
     data["_embedded"]["products"].forEach((product) => {
       document.write(`<h1>${product.productName}</h1>`);
       document.write(`<p>${product["_links"]["self"]["href"]}</p>`)
+     
     });
   });
